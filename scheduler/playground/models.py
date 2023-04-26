d2d1a5792d91a255d6f1ae31e9afd98142e8b705
ksdb12 <kiril.voigtlaender@ru.nl>
2023-04-26 18:09:33 +0200

    Creation of a database

--- scheduler/playground/models.py
+++ scheduler/playground/models.py
@@ -1,3 +1,15 @@
 from django.db import models
+from django.db.models import Model
 
-# Create your models here.
+class Type(models.Model):
+    type_text = models.CharField(max_length=200)
+   
+
+class Event(models.Model):
+    name = models.CharField(max_length=50)
+    event_type = models.ForeignKey(Type, on_delete=models.CASCADE)
+    date = models.DateField()
+    start_time = models.TimeField()
+    end_time = models.TimeField()
+
+
