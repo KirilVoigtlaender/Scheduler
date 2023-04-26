396ca387761bd011d26e54a52ea8e8d79bb994fa
ksdb12 <kiril.voigtlaender@ru.nl>
2023-04-26 18:42:50 +0200

    Updated our database, added a repetition

--- scheduler/playground/models.py
+++ scheduler/playground/models.py
@@ -6,10 +6,19 @@ class Type(models.Model):
    
 
 class Event(models.Model):
+    REPETITION_VALUES = (
+        ('Never', 'Never'),
+        ('Every Day', 'Every Day'),
+        ('Every Week', 'Every Week'),
+        ('Every Month', 'Every Month'),
+        ('Every Year', 'Every Year')
+    )
+
     name = models.CharField(max_length=50)
     event_type = models.ForeignKey(Type, on_delete=models.CASCADE)
     date = models.DateField()
     start_time = models.TimeField()
     end_time = models.TimeField()
-
+    repetition = models.CharField(choices= REPETITION_VALUES, max_length=20, default='Never') #In the add task,add appointment it 
+                                                               #is named as importancy level
 
