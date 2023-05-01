11b566cd763c387908ee190e2a4cd89baf0e58a0
Ping Mei <ping-mei@outlook.com>
2023-05-02 01:38:09 +0200

    hallo

--- scheduler/playground/urls.py
+++ scheduler/playground/urls.py
@@ -2,5 +2,9 @@ from django.urls import path
 from . import views
 
 urlpatterns = [
-    path('hello/', views.say_hello)
+    path("", views.index, name='base'),
+    # # path("home", views.home, name='home'),
+    # # path('add_task', views.add_task),
+    # # path('add_appointment', views.add_appointment),
+    path('website/', views.website),
 ]
--- scheduler/playground/views.py
+++ scheduler/playground/views.py
@@ -1,6 +1,10 @@
 from django.shortcuts import render
 from django.http import HttpResponse
-
+# from django.template import loader
 
 def say_hello(request):
-    return HttpResponse('Hello World')
+    return HttpResponse('hello world')
+
+# def website(request):
+#     template = loader.get_template('website.html')
+#     return HttpResponse(template.render(request))
