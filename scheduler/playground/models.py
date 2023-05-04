f7c16f0c40f7b51a54d4d2fad60e29624c2c9a36
kareemdequillettes <slimmeriq@gmail.com>
2023-05-05 01:51:42 +0200

    a little more schedule

--- scheduler/playground/models.py
+++ scheduler/playground/models.py
@@ -1,3 +1,24 @@
 from django.db import models
+from django.db.models import Model
 
-# Create your models here.
+
+class Appointment(models.Model):
+    REPETITION_VALUES = (
+        ('Never', 'Never'),
+        ('Every Day', 'Every Day'),
+        ('Every Week', 'Every Week'),
+        ('Every Month', 'Every Month'),
+        ('Every Year', 'Every Year')
+    )
+
+    name = models.CharField(max_length=50)
+    start_time = models.TimeField()
+    end_time = models.TimeField()
+    date = models.DateField()
+    #repetition = models.CharField(choices= REPETITION_VALUES, max_length=20, default='Never')
+    repetition = models.PositiveSmallIntegerField(choices=(
+        (1,"Never"),
+        (2,"Every day"),
+        (3,"Every Week"),
+        (4,"Once a month")
+    ))
--- scheduler/playground/views.py
+++ scheduler/playground/views.py
@@ -1,9 +1,18 @@
 from django.shortcuts import render
 from django.http import HttpResponse
+from django.template import loader
+from .models import Appointment
+from django.db import models
 
 def say_hello(request):
     return HttpResponse('hello world')
 
 def website(request):
-    return render(request, 'website.html')
+    appointments = Appointment.objects.all().values
+    template = loader.get_template('website.html')
+    context = {
+        'appointments': appointments,
+        'date': models.DateField('2023-07-20'),
+    }
+    return HttpResponse(template.render(context, request))
 
