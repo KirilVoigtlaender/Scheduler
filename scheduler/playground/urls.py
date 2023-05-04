7a0f547208f99bf7225c7c969a4b6b7b167cc521
Ping Mei <ping-mei@outlook.com>
2023-05-04 23:38:18 +0200

    Skeleton design of the schedule

--- scheduler/playground/urls.py
+++ scheduler/playground/urls.py
@@ -2,9 +2,7 @@ from django.urls import path
 from . import views
 
 urlpatterns = [
-    path("", views.index, name='base'),
-    # # path("home", views.home, name='home'),
-    # # path('add_task', views.add_task),
-    # # path('add_appointment', views.add_appointment),
-    path('website/', views.website),
+    path('hello/', views.say_hello),
+    path('website/', views.website)
+
 ]
--- scheduler/playground/views.py
+++ scheduler/playground/views.py
@@ -1,10 +1,9 @@
 from django.shortcuts import render
 from django.http import HttpResponse
-# from django.template import loader
 
 def say_hello(request):
     return HttpResponse('hello world')
 
-# def website(request):
-#     template = loader.get_template('website.html')
-#     return HttpResponse(template.render(request))
+def website(request):
+    return render(request, 'website.html')
+
