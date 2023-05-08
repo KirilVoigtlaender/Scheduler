b1aa5e9372580f8bfaf937cb57f6f977eeddc571
ksdb12 <kiril.voigtlaender@ru.nl>
2023-05-08 15:22:28 +0200

    Continued on creating an admin site

--- scheduler/playground/admin.py
+++ scheduler/playground/admin.py
@@ -1,3 +1,11 @@
 from django.contrib import admin
 
 # Register your models here.
+from .models import Task,Appointment
+
+class TaskAdmin(admin.ModelAdmin):
+    list_display = ['name']
+admin.site.register(Task,TaskAdmin)
+class AppointmentAdmin(admin.ModelAdmin):
+    list_display = ['name']
+admin.site.register(Appointment,AppointmentAdmin)
