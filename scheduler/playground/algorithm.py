d59195e88805e9b064af8fe29c8476cafede24d4
ksdb12 <kiril.voigtlaender@ru.nl>
2023-06-04 20:42:29 +0200

    fixed the blocks in the schedule
    Actually the algorithm was wrong

--- scheduler/playground/algorithm.py
+++ scheduler/playground/algorithm.py
@@ -107,6 +107,7 @@ def algorithm():
                     for slot in range(96 - task_slots + 1):  # Iterate over all possible starting slots
                         for i in range(task_slots):
                             if filled_schedule[max_free_day][slot+i] == False:
+                                
                                 start_slot = slot
                                 is_looping = False
                             #    print("Kareem says we stopped the loop.")
@@ -122,10 +123,12 @@ def algorithm():
                     # Update the filled_schedule array to mark the time slots as occupied
                     for slot in range(start_slot, start_slot + task_slots):
                         filled_schedule[max_free_day][slot] = sorted_tasks[task].name
+                        print(slot, max_free_day)
 
                     # Update the freetime array to subtract the occupied time slots
                     freetime[max_free_day] = freetime[max_free_day]- (task_slots * 15)
                     tasktime = tasktime - task_slots
+                    print(tasktime)
                     if tasktime <= 0:
                         break
   
