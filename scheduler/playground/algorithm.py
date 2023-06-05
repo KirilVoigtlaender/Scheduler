a26cbd63612973b98e9c072df7105e0b167741fb
ksdb12 <kiril.voigtlaender@ru.nl>
2023-06-05 14:24:26 +0200

    Algorithm works
    Ping Mei and Kareem don't you dare to fuck it up

--- scheduler/playground/algorithm.py
+++ scheduler/playground/algorithm.py
@@ -99,16 +99,16 @@ def algorithm():
                     else:
                         task_slots = 1
                     start_slot = None
-                    is_looping = True
                     for slot in range(96 - task_slots + 1):  # Iterate over all possible starting slots
+                        all_available = True
                         for i in range(task_slots):
-                            if filled_schedule[max_free_day][slot+i] == False:
-                                start_slot = slot
-                                is_looping = False
+                            if filled_schedule[max_free_day][slot+i] != False:
+                                all_available = False
                             #    print("Kareem says we stopped the loop.")
                                 break
-                        if is_looping == False:
-                        #    print("Happy clapping there is no loop.")
+
+                        if all_available:
+                            start_slot = slot
                             break
 
                         # if all(not filled_schedule[max_free_day][slot+i] for i in range(task_slots)):
@@ -118,7 +118,7 @@ def algorithm():
                     # Update the filled_schedule array to mark the time slots as occupied
                     for slot in range(start_slot, start_slot + task_slots):
                         filled_schedule[max_free_day][slot] = sorted_tasks[task].name
-                        print(slot, max_free_day)
+                        print("here", slot, max_free_day, tasktime)
 
                     # Update the freetime array to subtract the occupied time slots
                     freetime[max_free_day] = freetime[max_free_day] - (task_slots * 15)
